ca7fb1bce79ae6033430d69d56919a0f6abaeaad
Kelvin Melendez <kpisolucionesrd@gmail.com>
2019-02-07 13:00:22 -0800

    Cambios

---
+++ componentes/ElementosCompactos/radioBottom.js
@@ -0,0 +1,40 @@
+//Librerias globales
+import React, {Component} from 'react';
+import {StyleSheet, View,Text} from 'react-native';
+import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-native-simple-radio-button';
+
+export default class RadioBottom extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      valores:[
+        {label: 'Si', value: 'Si' },
+        {label: 'No', value: 'No' }
+      ],
+    }
+  }
+  //Funciones
+  cambioValor=(itemValue)=>{
+    this.props.funcion(this.props.identificacion,itemValue);
+  }
+  render(){
+    return(
+      <View>
+      <Text style={RadioBottomStyle.text_subitem}>{this.props.identificacion}</Text>
+        <RadioForm
+          radio_props={this.state.valores}
+          initial={-1}
+          onPress={this.cambioValor}
+        />
+      </View>
+    )
+  }
+};
+
+const RadioBottomStyle=StyleSheet.create({
+  text_subitem:{
+    color:'black',
+    fontWeight:'bold',
+    fontSize:15,
+  }
+});
